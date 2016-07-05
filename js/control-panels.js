b185ebcdede593aadc88331c3a0a59b51cf58724
Anton Eremin <antoremin@gmail.com>
2016-07-05 14:38:22 +0300

    Add files via upload

--- js/control-panels.js
+++ js/control-panels.js
@@ -1,1 +1,11 @@
+var choices = document.querySelectorAll('.panel');
+var maxWidth = 0;
+// read
+for (i = 0; i < choices.length; ++i) {
+    maxWidth = Math.max(maxWidth, choices[i].offsetWidth)
+};
 
+// write
+for (i = 0; i < choices.length; ++i) {
+    choices[i].style.width = maxWidth + "px";
+};
